382c0e2e51fb63adad635555f7f933edef4a4385
tamerlanstt <tamerlan.stt@gmail.com>
2022-05-15 23:49:08 +0200

    commit

---
+++ script.js
@@ -0,0 +1,173 @@
+let cities = {
+  "Nur-Sultan": {
+    desc: "New center with wide avenues and unusual buildings was built from scratch on the left bank of the Ishim River; whereas the right bank of the river is calm and cozy atmosphere, still preserves the memory of the past.",
+    in: [
+      {title: "Baiterek", photo: "nursultan/n1.jpg", map: 'e1bJ3UVUDbUs8iATA', text: "A monument and observation tower. A tourist attraction popular with foreign visitors and Kazakhstanis, it is emblematic of the city, which became capital of the country in 1997."},
+      {title: "Hazrat Sultan Mosque", photo: "nursultan/n2.jpg", map:'sEwmiHL1G1vpTUJk8', text: `The largest mosque in Central Asia. The building was constructed in classical Islamic style with traditional Kazakh ornaments. Located on the right bank of the Yesil river the Mosque is adjacent to the Palace of Peace and Reconciliation, the monument "Kazakh Eli" and the Independence Square. It can accommodate five thousand worshipers, and on holidays - up to 10 thousand people. The area of the mosque across more than 11 hectares and construction area of 17,700 square meters.`},
+      {title: "EXPO-2017", photo: "nursultan/n3.jpg", map: 'iKEQJbSkVD7avp5T6', text: `The expo's theme was "Future Energy", and aimed to create a global debate between countries, nongovernmental organizations, companies and the general public on the crucial question: "How do we ensure safe and sustainable access to energy for all while reducing CO2 emissions?". The Expo 2017 grounds and pavilion space have been converted into commercial space for corporate, startups, education and research entities.`},
+      {title: "National Museum", photo: "nursultan/n4.jpg", map: 'PonwD7TX8kLgT2j46', text: `The museum is located on the main square of the country - the Independence Square. Many values identified during the "Cultural Heritage" State Program constitute the invaluable fund of the National Museum of Kazakhstan. The museum building is an eye catcher with its unusual external form. The largest unique museum complex has an area of 74,000 sq.m. and consists of seven blocks with a variable number of storeys to the ninth floor. `},
+      {title: "Astana Opera", photo: "nursultan/n5.jpg", map: 'heNKt6x19U51KSzV7', text: `It is the second newly built opera house in Astana, as the Palace of Peace and Reconciliation, hosting a 1500 seats opera hall, was completed in 2006. It is considered the third biggest opera house in the world;[2] one of its remarkable characteristics is the 1.6 ton chandelier hanging in the 13 m high concourse. The opera house has also several facilities, such as a restaurant and all installations required for the troupe's rehearsals.`},
+      {title: "Khan Shatyr", photo: "nursultan/n6.jpg", map: 'J2pBiFvQ8r6cP6tk6', text: `The 90-metre high tent has a 200-by-195-metre elliptical base covering 140,000 square metres built in a neofuturist style. Under the tent, an area larger than 10 football stadiums, is an urban-scale park, shopping and entertainment venue with squares and cobbled streets, a boating river, shopping centre, minigolf and indoor beach resort.`},
+      {title: "MEGA Silkway", photo: "nursultan/n7.jpg", map: 'XnjdGiQSihyzcsx16', text: `Mega Silk Way is a gateway development to the EXPO 2017 site. Containing the country’s most extensive range of shopping and entertainment facilities, the scheme includes restaurants, a food court, a 10-screen cinema (including an IMAX screen) and many other leisure offers.`},
+      {title: "Presidental Park", photo: "nursultan/n8.jpg", map: 'r1HQxTTY4sHv4Liw8', text: `One of the largest urban parks in Nur-Sultan. Located right behind the Ak Orda Presidential Palace and across the Ishim river. It is the site of the Palace of Peace and Reconciliation with flower beds and trees occupying most of the territory. The park is surrounded by multi-story residential buildings and it's artificial stream with fountains is shaped as the Simurgh, a bird symbol of Kazakhstan's independence.`},
+      {title: "Central Park", photo: "nursultan/n9.jpg", map: '5wdNhbJa1seVqovq5', text: `The park is a great public place for spending time with family or friends. Whether it’s at the height of summer or in the middle of winter, the park offers a great variety of outdoor activities. It features jogging and biking paths, and even a swimming pool. In winter, the park offers many paths for cross-country skiing, and ice skating rinks are also available. Equipment rental facilities are open all year round: in winter you can rent skis and skates, in summer – bicycles, scooters, roller skates, and kid’s electric cars.`},
+    ],
+    out: [
+      {title: "Korgalzhyn Nature Reserve", photo: "nursultan/o1.jpg", map: 'fyS7wL1GG7AvPYAP7', text: `Nature reserve in Aqmola and Karaganda regions of Kazakhstan, located west of the city of Nur-Sultan. It is part of the UNESCO heritage site Saryarka — Steppe and Lakes of Northern Kazakhstan, Ramsar site and biosphere reserve. It protects about 5432 km2 of lakes, steppes and semi deserts. The biggest lake is Lake Tengiz.`},
+    ],
+    upd: "2022 April"
+  },
+  "Almaty": {
+    desc: "The city is framed by snowcapped mountains covered with spruces creating a stunning scenery year round. The world-class ski resort can be reached in just 30 minutes from the city center",
+    in: [
+      {title: "Kok tobe", photo: "almaty/a2.jpg", map: 'hYYVm6usbhnEGChB8', text: `There is a popular recreation area on top of the mountain. The mountain's height is 1100 meters above sea level. Kök Töbe is one of the main landmarks in the city, and it is popular among visitors and tourists to Almaty.`},
+      {title: "Medeu", photo: "almaty/a3.jpg", map: 'QoL5mcoe17UF73Gq8', text: `outdoor speed skating and bandy rink. It is located in a mountain valley (Medeu Valley, or the valley of Malaya Almatinka River) on the south-eastern outskirts of Almaty, Kazakhstan. Medeu sits 1,691 metres above sea level. It consists of 10.5 thousand square meters of ice and utilizes a sophisticated freezing and watering system to ensure the quality of the ice.`},
+      {title: "Abay Opera House", photo: "almaty/a5.jpg", map: 'AE68ykz1PDoiZCBb9', text: "Opera and Ballet Theater in Alma-Ata, which is one of the leading opera houses in Kazakhstan"},
+      {title: "Green Bazaar", photo: "almaty/a6.jpg", map: 'sqkbd9EZKEZKn3KT9', text: "The market currently sells spices, prepared foods, produce, textile, furniture, and a wide variety of different objects, including antiques. The area originally served as a guest yard for traveling merchants and caravans. In 1887, Almaty suffered a major earthquake, which destroyed most of the area (including the bazaar). In 1927, the bazaar was reconstructed and received the name Central Farm Market. However, it eventually became known as the Green Bazaar. The bazaar is currently a major tourist attraction in Almaty."},
+      {title: "Central Mosque", photo: "almaty/a7.jpg", map: 'tG1n9jiLwbLfysSr8', text: "One of the largest mosques in Almaty, and in Kazakhstan. Designed for 7000 worshippers, it was built on the site of the old mosque that dated to 1890 and caught fire in 1987."},
+      {title: "Botanical Garden", photo: "almaty/a8.jpg", map: 'vkGwL8uK22b4ucAg6', text: "Its area occupies more than a hundred hectares. It contains a live exhibition of different species of plants coming not only from our country, but also from Russia, the Caucasus, East Asia and North America. It is not necessary to travel to see plants from all over the world - just visit the botanical garden in Almaty. There are some very rare species of trees and herbaceous plants, which, thanks to proper care, grow and bloom actively."},
+    ],
+    out: [
+      {title: "Big Almaty Lake", photo: "almaty/a1.jpg", map: '8bqeHH9TggA5vGgq7', text: `Natural alpine reservoir. It is located in the Trans-Ili Alatau mountains, 15 km south from the center of Almaty in Kazakhstan. The lake is 2511 meters above sea level. The lake is a major source of drinking water for the region. People can access the lake by car (approximately 1 hour drive from the city center), bike, or hiking (approximately a half-day trip). The lake is 15 km away from the city. Access to the lake requires payment of a fee to enter the national park (~450KZT in October 2019). The lake's average water temperature is 8-10 C, and swimming is prohibited as the lake is the main source of drinking water for Almaty. `},
+      {title: "Shymbulak", photo: "almaty/a4.jpg", map: 'MGMmXeMrLNqQBFTE7', text: `The largest ski resort in Central Asia. It is located in the upper part of the Medeu Valley in the Zailiisky Alatau mountain range, at an elevation of 2,200 metres (7,200 ft) above sea level. The resort area is about 25 kilometres (16 mi) south of Almaty city by Medeo road. It is popular for its mild climate, large quantity of sunny days and great amount of snow through the winter (from November till May).`},
+    ],
+    upd: "2022 May"
+  },
+  "Shymkent": {
+    desc: "Nowadays Shymkent is the largest city in Kazakhstan in terms of area. Known from ancient times as a flourishing city on the Great Silk Road. The city’s name is translated as a ‘garden city’",
+    in: [
+      {title: "Central park", photo: "shymkent/s1.jpg", map: 'KxFKStLYeG6u6o3B6', text: "One of the favorite places for tourists. All conditions for vacationers are created here. A neat lawn, flower beds, and well-groomed alleys make the park one of the favorite places for evening walks. The park also has a playground with swings, slides, and a maze. Additionally there is an outdoor gym."},
+      {title: "Zoo of Shymkent", photo: "shymkent/s2.jpg", map: '2XJVSDuZ8pUbAL6G7', text: "The state zoo of the city Shymkent in Kazakhstan.[1] It is one of the largest and oldest zoological parks in the country. From the total zoo's area of 54 hectares (130 acres), under an exposition 34 hectares (84 acres). The Shymkent Zoo contains more than 20 kinds of animals brought in the 'Red book'. 25% of requirements of forages for animals the zoo grow up on own earth, the area of 30 hectares (74 acres). "},
+      {title: "Dendrological Park", photo: "shymkent/s3.jpg", map: 'yqu2tj8z2tXHje716', text: "The park opened in 1979 and since then it has been working towards preserving the nature. Here you can see a great variety of plants and animals, including peacocks, squirrels, and deer. In the center of the park there is a picturesque lake with a catamaran station. The park territory is huge, so there is a bicycle rental and bike paths. Shymkent Dendrological Park can be undoubtedly called the crown jewel of the region and a landmark of Kazakhstan"},
+      {title: "Ancient citadel", photo: "shymkent/s7.jpg", map: 'TZLKPmTpGVtvQ47z8', text: `The citadel was built to improve the defense of the city of Sairam, which was important for the passage of the Silk Road. It occupied 4 hectares, was surrounded by protective walls and ditches, and hosted a well-armed military base.`},
+      {title: "Sayramsu", photo: "shymkent/s9.jpg", map: 'LsWVwmQ7cj9CUpPw8', text: "It is a river of southern Kazakhstan. It is a tributary of the Badam near Shymkent."},
+    ],
+    out: [
+      {title: "Arystan Bab Mausoleum", photo: "shymkent/s4.jpg", map: 'ixCMpKZKMSJsjXfW6', text: "The mausoleum features a large central arch and wide front facade with minarets at the ends and two large domes to the left of the main arch. As well as the two-chambered table-tomb (gurkhana) of Arystan Bab and three of his students, Hermet-Azyra, Karga-Baba and Lashyn-Baba, a mosque and auxiliary quarters and museum are located in the other rooms of the mausoleum."},
+      {title: "Sayram-Ugam National Park", photo: "shymkent/s5.jpg", map: 'jAgk9WFuQdss1L6X7', text: `A mountainous region of the Western Tian Shan Mountains, on the border with Uzbekistan. Ugam-Chatkal National Park of Uzbekistan is across the border, and Aksu-Zhabagly Nature Reserve of Kazakhstan is directly to the northeast along the border. The area has high levels of species diversity, covering floral communities from steppe to high altitude zones. It is particularly known for stands of juniper forests, and stands of fruit and nut trees.`},
+      {title: "Birgulyuk", photo: "shymkent/s6.jpg", map: '4xuxiosnAazQWUA98', text: `the westernmost spur of the Tien Shan mountain range, famous for its height of more than 7000 meters. The river valley, as if squeezed between the spurs, due to the increase in stunningly beautiful landscapes. The humidity here is so high that the soil is a continuous grassy meadow.`},
+      {title: "Aksu Canyon", photo: "shymkent/s8.jpg", map: 'VmBiWv8U5keBpgs88', text: `The canyon is 15 km long and 500m deep canyon and located in southern Kazakhstan in the Turkistan region in the north-west of the Tian Shan mountain range and east of the outskirts of one-million-people city Shymkent. Aksu Canyon is one of the largest canyons in Central Asia and beside the Sharyn Canyon one of the most popular canyons in Kazakhstan. It has an alpine character with nearby mountains reaching over 4000m.`},
+    ],
+    upd: "2022 March"
+  },
+  "Other": {
+    desc: "Consider visiting other places which may also interest you",
+    in: [
+      {title: "Turkistan", photo: "shymkent/s10.jpg", map: '7F1NPodULf4Xaouo7', text: `City and the administrative center of Turkistan Region of Kazakhstan, near the Syr Darya river. It is situated 160 km north-west of Shymkent on the Trans-Aral Railway between Kyzylorda to the north and Tashkent to the south. Turkistan's most prominent historical and cultural asset is the Mausoleum of Khoja Ahmed Yasawi, a UNESCO World Heritage site.`},
+    ],
+    out: [],
+    upd: "2022 March"
+  }
+}
+
+window.onload = function () {
+  let loc = window.location.pathname.split("/").pop();
+  if (loc == "visit.html") {
+    for (var i = 0; i < Object.keys(cities).length; i += 1) {
+      var item = cities[Object.keys(cities)[i]]
+      document.getElementById(i + "-ins").innerHTML = item.in.length
+      document.getElementById(i + "-out").innerHTML = item.out.length
+      document.getElementById(i + "-upd").innerHTML = item.upd
+    }
+    let hash = window.location.hash.replace('#', '');
+    if (hash in cities) {
+      document.getElementById('selectcity').value = hash;
+      showcity();
+      document.getElementById('city').scrollIntoView();
+    }
+  }
+  else if (loc == "media.html") {
+    let photos = document.getElementById('photos')
+    for (var i = 0; i < Object.keys(cities).length; i += 1) {
+      var city = Object.keys(cities)[i]
+      var item = cities[city]
+      for (var j = 0; j < item.in.length; j += 1) {
+        var card = document.createElement('div');
+        card.className = "card col-lg-4 col-md-6 p-0"
+        card.innerHTML = `
+          <img alt="place in ${city}" class="card-img visitIMG forceprint" src="media/${item.in[j].photo}">
+          <div class="card-img-overlay">
+            <span class="media-desc">${city}</span>
+          </div>
+        `
+        photos.append(card);
+      }
+    }
+  }
+}
+
+function showcity() {
+  let city = document.getElementById('selectcity').value;
+  let outside = document.getElementById('outside').checked;
+  if (city in cities) {
+    let div = document.getElementById('city')
+    div.className = "card mt-4";
+    div.innerHTML = `
+      <div class="container">
+        <h1 style="text-align: center;">${city}</h1>
+        <p>${cities[city]['desc']}</p>
+    `
+    for (var i = 0; i < cities[city]['in'].length; i += 1) {
+      let current = cities[city]['in'][i];
+      div.innerHTML += `
+        <div class="list-group list-group-flush">
+          <div class="row p-2">
+            <div class="col-md-6 col-lg-3 noprint" style="height: 200px; overflow: hidden;">
+              <a href="media/${current.photo}">
+                <img alt="${city}, ${current.title}" class="visitIMG" src="media/${current.photo}" alt="">
+              </a>
+            </div>
+            <div class="col-md-8 col-lg-9">
+              <h2>${current.title}</h2>
+              <p>${current.text}</p>
+              <p>Show on map: <a href="https://goo.gl/maps/${current.map}">Google Maps</a>.</p>
+            </div>
+          </div>
+        </div>
+      `
+    }
+    if (outside) {
+      for (var i = 0; i < cities[city]['out'].length; i += 1) {
+        let current = cities[city]['out'][i];
+        div.innerHTML += `
+          <div class="list-group list-group-flush">
+            <div class="row p-2">
+              <div class="col-md-6 col-lg-3 noprint" style="height: 200px; overflow: hidden;">
+                <a href="media/${current.photo}">
+                  <img class="visitIMG" src="media/${current.photo}" alt="">
+                </a>
+              </div>
+              <div class="col-md-8 col-lg-9">
+                <h2>${current.title} (outside)</h2>
+                <p>${current.text}</p>
+                <p>Show on map: <a href="https://goo.gl/maps/${current.map}">Google Maps</a>.</p>
+              </div>
+            </div>
+          </div>
+        `
+      }
+    }
+    div.innerHTML += '</div>'
+  }
+}
+
+function sendMessage() {
+  var text = ['name', 'email', 'phone', 'message'];
+  var data = text.map(a => document.getElementById(a).value);
+  data.push(document.querySelector('input[name="pref"]:checked').value);
+  data.push(document.getElementById('news').checked);
+  let pref = (data[4] == "useEmail" ? "email" : "phone");
+  let message = `Dear, ${data[0]}. \n\nThanks for your feedback: \n${data[3]}. \n\nWe will contact with you using provided ${pref}. \nContact details accepted: ${data[1]} ${data[2]}.`;
+  Email.send({
+    Host : "smtp.mail.yahoo.com",
+    Username : "tamerlan.web@yahoo.com",
+    Password : "Tamerlan08",
+    To : data[1],
+    From : "tamerlan.web@yahoo.com",
+    Subject : "Feedback received!",
+    Body : message
+  }).then(
+    message => console.log(message)
+  );
+  alert(message);
+}
